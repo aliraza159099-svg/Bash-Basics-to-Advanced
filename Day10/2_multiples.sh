47443cda258bb6ff6ae0f03bbc8b88e710c8b29f
Ali Raza <aliraza159099@gmail.com>
2026-09-05 17:42:41 +0500

    Modified the multiplication

--- Day10/2_multiples.sh
+++ Day10/2_multiples.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function mul(){
-	for (( i=1 ; i<=10 ; i++))
+	for (( i=1 ; i<=$limit ; i++))
 	do
 		echo "$1 x $i = $(( $1*i))"
 	done
@@ -11,6 +11,8 @@ do
 	read -p "Enter your number : " num
 	if [ $num -ne 0 ] 
 	then
+		read -p "Enter your limit : " limit
+		echo "=== Multiplication Table of $num ==="
 	mul "$num"
 else
 	echo "=== Thanks for using AR Calculator ==="
