6d34a53c78207f9e97e84358242dc03b3bafba2f
Ali Raza <aliraza159099@gmail.com>
2026-09-05 17:25:31 +0500

    Changed in and operator

--- Day10/and_operator.sh
+++ Day10/and_operator.sh
@@ -3,9 +3,9 @@
 name="ali"
 pass="1234"
 
-if [ name=="ali" ] && [ pass=="1234" ];
+if [ "$name" == "ali" ] && [ "$pass" == "1234" ];
 then
 	echo "You are allowed"
 else
-	echo "You are not allowed"
+	echo "You are not allowed "
 fi
