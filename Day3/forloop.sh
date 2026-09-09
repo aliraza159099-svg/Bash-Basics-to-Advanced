c538714b197c61c64bce0428a8c492f94d85dc87
Ali Raza <aliraza159099@gmail.com>
2026-09-09 22:13:36 +0500

    Work on previous concepts

--- Day3/forloop.sh
+++ Day3/forloop.sh
@@ -2,12 +2,11 @@
 
 #Using for loop to automate things 
 
-
 # read -p "Enter the number of files: " num
 # read -p "Enter the name of files: " name
-for (( i = 1 ; i <= $2 ; i++ ));
+for (( i = 1 ; i <= $2 ; i++ ))
 do
 	mkdir $1$i
 done
 
-ls
+
